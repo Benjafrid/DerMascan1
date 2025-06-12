058afe4b0def81c629e4a3f9e4058cf382979dd4
FeliCres7 <48242703@est.ort.edu.ar>
2025-06-11 22:00:47 -0300

    .

--- IA/api.py
+++ IA/api.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, HttpUrl
 import requests
 from PIL import Image
 import io
 import random
 
+# pip install fastapi uvicorn requests Pillow
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class InputData(BaseModel):
     image_url: HttpUrl | None = None
@@ -21,7 +33,7 @@ async def predict(data: InputData):
         try:
             response = requests.get(data.image_url)
             response.raise_for_status()
-            Image.open(io.BytesIO(response.content))
+            Image.open(io.BytesIO(response.content))  # validar imagen
             image_risk = random.randint(0, 99)
         except Exception as e:
             raise HTTPException(status_code=400, detail=f"Error al procesar la imagen: {e}")
@@ -37,3 +49,7 @@ async def predict(data: InputData):
         "riesgo_según_diametro": number_risk,
         "riesgo_general": general_risk
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
